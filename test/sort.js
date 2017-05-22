6614056a04018291c6706996389a733f95133c9c
David Chambers <dc@davidchambers.me>
2017-05-22 06:59:50 +0200

    remove logging

--- test/sort.js
+++ test/sort.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var Z = require('sanctuary-type-classes');
-
 var S = require('..');
 
 var eq = require('./internal/eq');
@@ -17,18 +15,7 @@ test('sort', function() {
   eq(S.sort(['foo', 'bar', 'baz']), ['bar', 'baz', 'foo']);
   eq(S.sort([S.Left(0), S.Right(0), S.Left(1), S.Right(1)]), [S.Left(0), S.Left(1), S.Right(0), S.Right(1)]);
 
-  var lte = Z.lte;
-  var count = 0;
-  Z.lte = function(x, y) { count += 1; return lte(x, y); };
-
   eq(S.sort(S.range(0, 100)), S.range(0, 100));
-  console.log('sort(range(0, 100)):', count);
-  count = 0;
-
   eq(S.sort(S.reverse(S.range(0, 100))), S.range(0, 100));
-  console.log('sort(reverse(range(0, 100))):', count);
-  count = 0;
-
-  Z.lte = lte;
 
 });
